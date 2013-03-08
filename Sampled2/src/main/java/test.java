04e5b2b39e4289a29e08908be87d5421cc976f1a
Techguna <techguna@hotmail.com>
2013-03-08 14:06:50 +0530

    Initial Structure Created

---
+++ Sampled2/src/main/java/test.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Admin
+ * Date: 3/8/13
+ * Time: 12:30 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class test {
+}
